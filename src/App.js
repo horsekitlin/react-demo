cfe640676e9594b8dcd96b366b8d8b9afbc63a9b
Tomas Lin <horsekit1982@gmail.com>
2018-09-26 10:50:53 +0800

    add toggle checkbox

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import { Segment, Header, Divider } from 'semantic-ui-react';
 import TextField from './TextField';
+import ToggleCheckbox from './ToggleCheckbox';
+import { themeGrapefruit } from './color';
 
 class App extends Component {
   constructor(props){
@@ -14,12 +16,19 @@ class App extends Component {
     return (
       <Segment basic>
         <Segment>
+          <Header as='h1'>Toggle Checkbox</Header>
+          <Divider />
+          <ToggleCheckbox />
+          <Divider />
+          <ToggleCheckbox bgColor={themeGrapefruit} />
+        </Segment>
+        <Segment>
           <Header as='h1'>Text Input</Header>
           <Divider />
-          <TextField placeholder='hello' label='哈囉世界' value='' />
+          <TextField placeholder='hello' label='哈囉世界' value='' onChange={console.log}/>
           <Divider />
           <Header as='h2'>Error</Header>
-          <TextField placeholder='error' label='哈囉錯誤' value='' error={true} />
+          <TextField placeholder='error' label='哈囉錯誤' value='' error={true} onChange={console.log}/>
         </Segment>
       </Segment>
     );
---
+++ src/ToggleCheckbox.js
@@ -0,0 +1,50 @@
+import React, {Fragment} from 'react';
+import styled, { keyframes } from 'styled-components';
+import { textGrey, themeBlue, white } from './color';
+
+const Checkbox = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const Input = styled.input`
+  font-size: 30px;
+  appearance: none;
+  width: 3.5em;
+  height: 1.5em;
+  background: ${textGrey};
+  border-radius: 3em;
+  position: relative;
+  cursor: pointer;
+  outline: none;
+  transition: all .2s ease-in-out;
+
+  :checked {
+    background: ${({bgColor = themeBlue}) => bgColor};
+  }
+
+  :after {
+    position: absolute;
+    content: "";
+    width: 1.5em;
+    height: 1.5em;
+    border-radius: 50%;
+    background: ${white};
+    box-shadow: 0 0 .25em rgba(0,0,0,.3);
+    transform: scale(.7);
+    left: 0;
+    transition: all .2s ease-in-out;
+  }
+
+  :checked:after {
+    left: calc(100% - 1.5em);
+  }
+`;
+
+
+const ToggleCheckbox = ({bgColor}) =>
+  <Checkbox>
+    <Input type="checkbox" value={true} bgColor={bgColor} />
+  </Checkbox>
+
+export default ToggleCheckbox;
